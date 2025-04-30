f93cbaab766867618fc92f9934412ef5b66da7c0
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-30 12:46:09 -0400

    reated loading .tsx

---
+++ utils/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+    <span className="loading loading-spinner loading-xl text-indigo-600"></span>
+  </div>
+  )
+}
+
+export default loading
