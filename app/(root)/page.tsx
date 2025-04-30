50c51235af01a99ad3295149730562664c191afc
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-30 09:16:13 -0400

    catch statement where if laength is less then 1 print no games

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,11 +25,13 @@ export default function Home() {
       <h1 className="text-3xl font-semibold mb-6 text-center text-gray-800">
         Games
       </h1>
-
+  
       {loading ? (
         <div className="flex justify-center items-center h-64">
           <span className="loading loading-spinner loading-xl text-indigo-600"></span>
         </div>
+      ) : item.length === 0 ? (
+        <p className="text-center text-gray-600 text-lg">No games scheduled.</p>
       ) : (
         <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {item.map((game) => (
@@ -41,7 +43,7 @@ export default function Home() {
                 <h2 className="text-2xl font-bold text-indigo-600">Game Type:</h2>
                 <span className="text-2xl text-gray-800">{game.name}</span>
               </div>
-
+  
               <p className="text-gray-700 mb-1">
                 <span className="font-semibold">Location:</span> {game.location}
               </p>
@@ -68,4 +70,5 @@ export default function Home() {
       )}
     </div>
   );
+  
 }
