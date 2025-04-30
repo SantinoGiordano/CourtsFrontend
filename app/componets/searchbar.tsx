dc57883a50fe4edf051139eea266f56cd7542cd6
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-30 14:25:27 -0400

    searchbar

---
+++ app/componets/searchbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type Props = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBar: React.FC<Props> = ({ value, onChange }) => {
+  return (
+    <>
+      <input
+        list="game-types"
+        name="type"
+        value={value}
+        onChange={onChange}
+        placeholder="Select or type game type"
+        className="input input-bordered w-full"
+        required
+      />
+
+      <datalist id="game-types">
+        <option value="5 vs 5" />
+        <option value="4 vs 4" />
+        <option value="3 vs 3" />
+        <option value="21" />
+        <option value="Other" />
+      </datalist>
+    </>
+  );
+};
+
+export default SearchBar;
