8086bfe4a8e59198f66a85363ea1375cfdf9dd7e
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-02 16:46:51 -0400

    error created but not working currently still implementing

---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const error = () => {
+  return (
+    <div>
+        Sorry this page is still under construction
+    </div>
+  )
+}
+
+export default error
