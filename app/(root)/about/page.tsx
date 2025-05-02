3d0184499e2ad5e6298c0846fd195b4e9bb0f667
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-02 09:51:40 -0400

    instructions

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,7 +1,17 @@
-export default function About(){
-    return(
-        <div>
-           Rules 
-        </div>
-    )
+export default function About() {
+  return (
+    <div>
+      <p>How it work:</p>
+      <div>
+        Create a post by filling in the information in the{" "}
+        <a href="/Create">Create</a> page, let people know how many your
+        bringing and how meny youll need so they can plan accordingly.
+      </div>
+      <div>
+        Once a game post has been madde everyone can view it and see what time
+        youll be at the courts
+      </div>
+      <p>Misson</p>
+    </div>
+  );
 }
