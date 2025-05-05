23369c65b0fba7f644ba41964dff31b12ce49aad
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-05 10:13:15 -0400

    styled contact adding api key to email

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,8 +1,13 @@
-
 const page = () => {
   return (
-    <div>Come back at another time</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <div className="bg-gray-800 p-8 rounded-xl shadow-lg">
+        <h1 className="text-2xl font-semibold text-gray-100">
+          Come back at another time
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
