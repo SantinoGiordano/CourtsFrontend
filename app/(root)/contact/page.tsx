8b4b2cdcb439bdefe57517b972b68693c5f11d16
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-07 09:16:02 -0400

    added contact component

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,13 +1,5 @@
-const page = () => {
-  return (
-    <div className="flex items-center justify-center h-screen bg-black text-white">
-      <div className="bg-gray-800 p-8 rounded-xl shadow-lg">
-        <h1 className="text-2xl font-semibold text-gray-100">
-          Come back at another time
-        </h1>
-      </div>
-    </div>
-  );
-};
+import ContactForm from "@/app/componets/contactForm";
 
-export default page;
+export default function Contact(){
+  return <ContactForm/>
+}
