a1d61e5a6c471139ac30840e1a42dbd17b9f1a02
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-09 11:50:34 -0400

    added padding for the nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Nav />
-        <div className="pt-10" />
+        <div className="pt-15" />
 
         {children}
       </body>
