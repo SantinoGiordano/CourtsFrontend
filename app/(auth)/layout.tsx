edaa2182583432f3c653e3f2aef301ee3eb48f89
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-23 19:31:36 -0400

    changed styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,9 +8,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div
-      className="flex flex-col items-center justify-center min-h-screen"
-      >
+    <div>
       {children}
     </div>
   );
