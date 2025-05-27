e2b56d0e9f297dfc95a77248ee72612167262be8
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-27 13:01:19 -0400

    added lat lang to vars

--- types/type.tsx
+++ types/type.tsx
@@ -1,15 +1,17 @@
-export type GameItem ={
-    playershave: number;
-    playersneed: number;
-    _id: string,
-    status: boolean;
-    time?: Date;
-    name: string;
-    location?: string;
-    type: string;
-    description: string;
-  };
+export type GameItem = {
+  playershave: number;
+  playersneed: number;
+  _id: string;
+  status: boolean;
+  time?: Date;
+  name: string;
+  location?: string;
+  type: string;
+  description: string;
+  lat?: number;
+  lng?: number;
+};
 
 export type User = {
-  username: string
-}
+  username: string;
+};
