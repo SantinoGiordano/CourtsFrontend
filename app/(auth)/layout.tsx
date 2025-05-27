819be1accc3a0e8c9ff4ceaf837e26c322869384
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-27 13:00:58 -0400

    removed styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,11 @@
 // /auth/layout.tsx
 
-import React from 'react';
+import React from "react";
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
