f73f1e155a4ad5743e825be5b5bf6eaa5b394ecb
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-28 19:16:02 -0400

    created a teplate that gose to 13@gmail

--- app/components/EmailTemplate.tsx
+++ app/components/EmailTemplate.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 
 interface EmailTemplateProps {
-  firstName: string;
+  name: string;
+  email: string;
+  message: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  firstName,
+  name,
+  email,
+  message,
 }) => (
   <div>
-    <h1>Welcome, {firstName}!</h1>
+    <h1>Hello</h1>
+    <p><strong>Name:</strong> {name}</p>
+    <p><strong>Email:</strong> {email}</p>
+    <p><strong>Message:</strong> {message}</p>
   </div>
 );
