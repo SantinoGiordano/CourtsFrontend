feb41d410eb050c34b86f03f338524218ee9ebc4
SantinoGiordano <santinogiordano13@gmail.com>
2025-05-28 16:26:24 -0400

    changed color schema

--- app/componets/nav.tsx
+++ app/componets/nav.tsx
@@ -8,49 +8,49 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full bg-white shadow-md dark:bg-zinc-900 fixed z-100">
-      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+    <nav className="w-full bg-blue-600 shadow-md dark:bg-zinc-900 fixed z-100">
+      <div className=" text-white dark:text-white hover:text-blue-300 max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         <Link
           href="./Home"
-          className="text-xl font-bold text-zinc-800 dark:text-white"
+          className="text-xl font-bold text-white dark:text-white"
         >
           Game Center
         </Link>
 
-        <div className="hidden md:flex space-x-6">
+        <div className="text-white dark:text-white hover:text-blue-300 hidden md:flex space-x-6">
           <Link
             href="./Home"
-            className="text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="text-white dark:text-white hover:text-blue-300"
           >
             Find Games
           </Link>
           <Link
             href="./Create"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Create
           </Link>
           <Link
             href="/About"
-            className="text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="text-white dark:text-white hover:text-blue-300"
           >
             About
           </Link>
           <Link
             href="./statusReport"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Status Report
           </Link>
           <Link
             href="/contact"
-            className="text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="text-white dark:text-white hover:text-blue-300"
           >
             Contact
           </Link>
           <Link
             href="./"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Log Out
           </Link>
@@ -67,37 +67,37 @@ export default function Navbar() {
         <div className="md:hidden px-4 pb-3 space-y-2">
           <Link
             href="./"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Find Games
           </Link>
           <Link
             href="./Create"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Create
           </Link>
           <Link
             href="./About"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             About
           </Link>
           <Link
             href="./statusReport"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Status Report
           </Link>
           <Link
             href="./contact"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Contact
           </Link>
           <Link
             href="./"
-            className="block text-zinc-600 dark:text-zinc-200 hover:text-blue-500"
+            className="block text-white dark:text-white hover:text-blue-300"
           >
             Log Out
           </Link>
